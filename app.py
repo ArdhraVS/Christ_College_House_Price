ddff54f6c186a6b1de24e601a0a1b749c5e2488a
ArdhraVS <ardhra139@gmail.com>
2026-09-17 14:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ floors=st.number_input("Enter no of floors:", min_value=0 ,max_value=10, value=1
 if st.button("Predict"):
   if area<600 or area>3000:
     st.error("Area should be within the 600 - 3000")
-  elif (bedrooms<1 or bedroom>4):
+  elif (bedrooms<1 or bedrooms>4):
     st.error("Bedrooms should be within the range 1 - 4")
   elif floors<0 and floors>10:
     st.error("Floors should be within the range 0 - 10")
