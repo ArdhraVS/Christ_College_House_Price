30e2585b057349a036ed56c8d2fe162e14a32593
ArdhraVS <ardhra139@gmail.com>
2026-09-17 14:08:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ bedrooms=st.number_input("Enter no of bedrooms:", min_value=1 ,max_value=4, valu
 floors=st.number_input("Enter no of floors:", min_value=0 ,max_value=10, value=1)
 
 if st.button("Predict"):
-  prediction=model.predict([[area,bedrooms,floors]])
-  st.success(f"Predicted price: {prediction[0]:.2f} Lakhs")
+  if area<600 or area>3000:
+    st.error("Area should be within the 600 - 3000")
+  elif (bedrooms<1 or bedroom>4):
+    st.error("Bedrooms should be within the range 1 - 4")
+  elif floors<0 and floors>10:
+    st.error("Floors should be within the range 0 - 10")
+  else:
+    prediction=model.predict([[area,bedrooms,floors]])
+    st.success(f"Predicted price: {prediction[0]:.2f} Lakhs")
